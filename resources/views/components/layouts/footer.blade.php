647caef05f9085c2168d8a03dc76549b2318b4ca
Alvar Buenfil <alvarbuenfil@MacBook-Air-de-Alvar.local>
2024-01-19 15:10:49 -0600

    footer

--- resources/views/components/layouts/footer.blade.php
+++ resources/views/components/layouts/footer.blade.php
@@ -1,5 +1,44 @@
 <footer class="footer">
     <div class="container">
-        
+        <div class="row">
+            <div class="col-lg-4 col-md-6 col-6">
+                <ul class="navtab">
+                    <li>
+                        <a href="">inicio</a>
+                    </li>
+                    <li>
+                        <a href="">Experiencias</a>
+                    </li>
+                    <li>
+                        <a href="">bucketlist</a>
+                    </li>
+                    <li>
+                        <a href="">Contacto</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="col-lg-4 col-md-6 col-6">
+                <ul class="contactform">
+                    <li>
+                        <a href="">
+                            <span class="pe-1">
+                                <img src="{{asset('/img/whats.svg')}}" width="20" alt="">
+                            </span> contactanos <br> 9999999999
+                        </a>
+                        
+                    </li>
+                    <li>
+                        <a href="">
+                            <span class="pe-1">
+                                <img src="{{asset('/img/whats.svg')}}" width="20" alt="">
+                            </span> ENVÍANOS UN CORREO <br> 9999999999
+                        </a>
+                        
+                    </li>
+                    
+                </ul>
+            </div>
+            <div class="col-lg-4 col-md-6 col-12"></div>
+        </div>
     </div>
 </footer>
