8fcb93c684a91fb0fa0c3e14633bf687a13578be
Alfredo Gonzalez Marenco <alfredomarenco@boletea.com>
2025-02-10 23:49:54 -0600

    Cambios

--- resources/views/components/layouts/guest.blade.php
+++ resources/views/components/layouts/guest.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>{{$title ?? ''}} - Beyond Yucatan</title>
+    <title>{{ $title ?? '' }} - Beyond Yucatan</title>
     @stack('scss')
 </head>
 
@@ -12,7 +12,7 @@
 
     <x-layouts.nav />
     <main>
-        {{$slot}}
+        {{ $slot }}
     </main>
     <x-layouts.footer>
 
@@ -20,18 +20,27 @@
 </body>
 
 <script type="text/javascript">
-function googleTranslateElementInit() {
-	new google.translate.TranslateElement({pageLanguage: 'es', includedLanguages: 'ca,eu,gl,en,fr,it,pt,de', layout: google.translate.TranslateElement.InlineLayout.SIMPLE, gaTrack: true}, 'google_translate_element');
-        }
+    function googleTranslateElementInit() {
+        new google.translate.TranslateElement({
+            pageLanguage: 'es',
+            includedLanguages: 'ca,eu,gl,en,fr,it,pt,de',
+            layout: google.translate.TranslateElement.InlineLayout.SIMPLE,
+            gaTrack: true
+        }, 'google_translate_element');
+    }
 </script>
 
-<script type="text/javascript" src="https://translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
+<script type="text/javascript" src="https://translate.google.com/translate_a/element.js?cb=googleTranslateElementInit">
+</script>
 <script>
-    window.addEventListener("scroll", function(){
-    var nav = document.querySelector("nav");
-    nav.classList.toggle("dark", window.scrollY > 0, {passive: true} );
+    window.addEventListener("scroll", function() {
+        var nav = document.querySelector("nav");
+        nav.classList.toggle("dark", window.scrollY > 0, {
+            passive: true
+        });
 
     })
-   </script>
-   @stack('js')
+</script>
+@stack('js')
+
 </html>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -199,6 +199,7 @@
     @endif
     @push('js')
         <script src="https://cdn.jsdelivr.net/npm/glider-js@1.7.9/glider.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
         <script src="{{ asset('/js/glider.js') }}"></script>
     @endpush
 </x-layouts.guest>
