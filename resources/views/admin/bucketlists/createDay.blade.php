af7e226808076c0e9aa61dc369cfc7e65cf7998d
Alfredo Gonzalez Marenco <alfredomarenco@boletea.com>
2025-05-29 12:46:48 -0600

    Cambios

--- resources/views/admin/bucketlists/createDay.blade.php
+++ resources/views/admin/bucketlists/createDay.blade.php
@@ -10,7 +10,7 @@
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
                     <div>
-                        <form action="{{ route('admin.bucketlists.storeDay', $bucket) }}" method="post"
+                        <form action="{{ route('admin.bucketlists.storeDay', $bucket->id) }}" method="post"
                             enctype="multipart/form-data">
                             @csrf
                             @method('POST')
